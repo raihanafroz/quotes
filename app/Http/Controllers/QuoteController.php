cbc9381a0d08e2022f8635db6f5b0f1277c55719
Raihan Afroz <raihanafroz9@gmail.com>
2020-12-11 16:21:41 +0600

    User quote view

--- app/Http/Controllers/QuoteController.php
+++ app/Http/Controllers/QuoteController.php
@@ -2,9 +2,78 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Quote;
+use Illuminate\Database\QueryException;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class QuoteController extends Controller
 {
-    //
+  public function quote_add(Request $request)
+  {
+    if($request->isMethod('POST')){
+      $data = $request->all();
+      $validator = Validator::make($data, [
+        'text' => 'required|string',
+      ]);
+      if ($validator->fails()) {
+        return redirect()->back()
+          ->withErrors($validator)
+          ->withInput();
+      }
+      try {
+        $quote = new Quote();
+        $quote->author_id = auth()->id();
+        $quote->text = $data['text'];
+        if ($quote->save()) {
+          $status = '<div x-data="{ show: true }" x-show="show" class="w-11/12 md:w-3/5 bg-white my-2 rounded-r-md px-6 border-l-4 -ml-4 border-gray-100 bg-green-500">
+              <div class="flex items-center py-4">
+                  <i class="fas fa-check border-2 border-gray-200 px-2 rounded-full fill-current text-4xl font-light text-gray-200"></i>
+                  <div class="ml-5">
+                      <h1 class="text-lg font-bold text-gray-200">Congratulations!</h1>
+                      <p class="text-gray-300 my-0">You successfully added New Quote.</p>
+                  </div>
+                  <div>
+                      <button type="button"  @click="show = false"  class=" text-yellow-100">
+                          <span class="text-2xl">&times;</span>
+                      </button>
+                  </div>
+              </div>
+          </div>';
+          return redirect()->route('dashboard')->with('status', $status);
+        }
+        $status = '<div x-data="{ show: true }" x-show="show" class="w-11/12 md:w-3/5 bg-white my-2 rounded-r-md px-6 border-l-4 -ml-4 border-gray-100 bg-yellow-400">
+            <div class="flex items-center py-4">
+                <i class="fas fa-exclamation-circle rounded-full fill-current text-4xl text-gray-800"></i>
+                <div class="ml-5">
+                    <h1 class="font-bold text-gray-800 text-lg">Warning !!!</h1>
+                    <p class="text-gray-800 my-0 ">Quote adding not possible.</p>
+                </div>
+                <div>
+                    <button type="button" @click="show = false"  class=" text-yellow-100">
+                        <span class="text-2xl text-gray-800">&times;</span>
+                    </button>
+                </div>
+            </div>
+        </div>';
+        return redirect()->back()->withInput()->with('status', $status);
+      } catch (QueryException $e) {
+        $status = '<div x-data="{ show: true }" x-show="show" class="w-11/12 md:w-3/5 bg-white my-2 rounded-r-md px-6 border-l-4 -ml-4 border-gray-100 bg-yellow-400">
+            <div class="flex items-center py-4">
+                <i class="fas fa-exclamation-circle rounded-full fill-current text-4xl text-gray-800"></i>
+                <div class="ml-5">
+                    <h1 class="font-bold text-gray-800 text-lg">Warning !!!</h1>
+                    <p class="text-gray-800 my-0 ">Something went wrong.</p>
+                </div>
+                <div>
+                    <button type="button" @click="show = false"  class=" text-yellow-100">
+                        <span class="text-2xl text-gray-800">&times;</span>
+                    </button>
+                </div>
+            </div>
+        </div>';
+        return redirect()->back()->withInput()->with('status', $status);
+      }
+    }
+  }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,7 +8,37 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <x-jet-welcome />
+                <div class="grid grid-rows grid-flow-col gap-4 lg:pb-8">
+                    <div class="col-span-2 mx-auto  lg:pt-8">
+                        <table class="table-fixed">
+                            <thead>
+                            <tr>
+                                <th class="border border-emerald-800 px-4 py-2 text-emerald-700">Serial</th>
+                                <th class="border border-emerald-800 px-4 py-2 text-emerald-700">Text</th>
+                                <th class="border border-emerald-800 px-4 py-2 text-emerald-700">Created At</th>
+                                <th class="border border-emerald-800 px-4 py-2 text-emerald-700">Option</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach(auth()->user()->quotes as $quote)
+                                <tr>
+                                    <td class="border border-emerald-500 px-4 py-2 text-emerald-600 font-medium">{{ $loop->iteration  }}</td>
+                                    <td class="border border-emerald-500 px-4 py-2 text-emerald-600 font-medium">{{ (strlen($quote->text) > 75) ? substr($quote->text, 0, 75).'...' : $quote->text }}</td>
+                                    <td class="border border-emerald-500 px-4 py-2 text-emerald-600 font-medium">{{ date('F d, Y h:i A', strtotime($quote->created_at)) }}</td>
+                                    <td class="border border-emerald-500 px-4 py-2 text-emerald-600 font-medium">
+                                        <button type="button" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-2 py-1 bg-green-600 text-base font-medium text-white hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 sm:ml-3 sm:w-auto sm:text-sm">
+                                            Edit
+                                        </button>
+                                        <button type="button" class="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-2 py-1 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm">
+                                            Delete
+                                        </button>
+                                    </td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
