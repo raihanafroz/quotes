540e78a2f54002e57161668da4aa43d6629ece52
Raihan Afroz <raihanafroz9@gmail.com>
2020-12-11 15:35:39 +0600

    Qoute model and controller created

--- app/Models/User.php
+++ app/Models/User.php
@@ -58,4 +58,9 @@ class User extends Authenticatable
     protected $appends = [
         'profile_photo_url',
     ];
+
+  public function quotes()
+  {
+    return $this->hasMany('\App\Models\Quote', 'author_id', 'id');
+  }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-//  return mix('');
+  return auth()->user()->quotes();
     return view('welcome');
 });
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+Route::middleware(['auth:sanctum', 'verified'])->name('quote.')->prefix('quote')->group( function () {
+  Route::post('/add', [\App\Http\Controllers\QuoteController::class, 'quote_add'])->name('add');
+  Route::post('/edit', [\App\Http\Controllers\QuoteController::class, 'quote_edit'])->name('edit');
+  Route::post('/add', [\App\Http\Controllers\QuoteController::class, 'quote_add'])->name('add');
+});
